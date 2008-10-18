ffb17796429384223779eb6180dbf0b12c6f1bbf
hustos <hustos@a6257370-f324-0410-8f77-b916e15dc553>
2008-10-18 09:02:38 +0000

    Grammar check. Due to 64bit machine problem, cann't compile. page.h added. Add page.o to Makefile.

--- include/i386/memory.h
+++ include/i386/memory.h
@@ -3,16 +3,6 @@
 
 #include <i386/types.h>
 
-#define PAGE_OFFSET 0xC0000000
-#define PAGE_SIZE 0x1000
-#define PAGE_SHIFT 12
-
-typedef struct page{
-	int32_t ref_count;		// reference count
-	uint32_t index;		// page index
-	uint32_t* virtual;	// where this page maps to
-	uint32_t* padding;
-}page_t;
 
 
 void init_mm();
